5490bf3e40c8fc63886783f7c97a0eae30f5ec86
Jordan Conway <jconway@linuxfoundation.org>
2015-07-07 15:41:20 -0400

    Loading bacula::storage before bacula::client
    Change-Id: I5f1dc4876d72088318c4c1db9506d2c75b210d78
    Signed-off-by: Jordan Conway <jconway@linuxfoundation.org>

--- manifests/baculaserver.pp
+++ manifests/baculaserver.pp
@@ -2,4 +2,7 @@ class role::baculaserver inherits role {
   include ::profile::bacula::director
   include ::profile::bacula::storage
   include ::profile::nfs::client
+
+  Class['::profile::bacula::storage'] -> Class['::profile::bacula::client']
+
 }
