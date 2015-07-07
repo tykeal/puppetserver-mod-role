de587911247153ad71a7669753cf3907ae996ffc
Jordan Conway <jconway@linuxfoundation.org>
2015-07-07 16:15:01 -0400

    Revert "Loading bacula::storage before bacula::client"
    This reverts commit 5490bf3e40c8fc63886783f7c97a0eae30f5ec86.
    
    Change-Id: If1b4f2cd72dbb2ce1f4a23d0214ccfe1c20791aa
    Signed-off-by: Jordan Conway <jconway@linuxfoundation.org>

--- manifests/baculaserver.pp
+++ manifests/baculaserver.pp
@@ -2,7 +2,4 @@ class role::baculaserver inherits role {
   include ::profile::bacula::director
   include ::profile::bacula::storage
   include ::profile::nfs::client
-
-  Class['::profile::bacula::storage'] -> Class['::profile::bacula::client']
-
 }
