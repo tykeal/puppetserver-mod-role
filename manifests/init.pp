2a173af48b60c7d96ce3d7bb5d4e29f0fc245eb9
Clint Savage <herlo@linuxfoundation.org>
2015-09-09 16:07:01 +0000

    Revert "Added profile::rsyslog::client to init role"
    This reverts commit 9478edc8b9c8925d09221848c4206f3f79cb98d4.
    
    Change-Id: I92f0dc79604a1b592ba164c08c2ecc8d5e627d4f

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,7 +12,6 @@ class role {
   include profile::puppet::agent
   include profile::resolv_conf
   include profile::rkhunter
-  include profile::rsyslog::client
   include profile::screen
   include profile::smtp
   include profile::ssh::server
